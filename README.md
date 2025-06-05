c8c0c4334562012ad983a3c5cacc943f55577336
Guillaume Raffin <theelectronwill@gmail.com>
2025-06-05 20:49:18 +0200

    template init with Alumet 0.8.1-dev-snapshot
    This should help Geo at IRIT.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# alumet-agent-template
-Minimal example of a custom Alumet agent
+# Alumet agent template
+
+Example of a custom Alumet agent.
+
+## Key parts
+
+This is a Rust project managed by [Cargo](https://doc.rust-lang.org/cargo/).
+It creates a binary app, defined in `src/main.rs`.
+
+- `Cargo.toml` declares a dependency on `alumet` (Alumet core), some Alumet plugins, and some third-party libraries.
+- `main.rs` performs a complete setup of Alumet, as it:
+  - loads the plugins
+  - loads the configuration file
+  - applies default configs if needed
+  - creates the measurement pipeline
+  - runs until you press Ctrl+C (or send SIGTERM in another way).
+
+Command-line arguments are not provided.
+To do that, we suggest you to have a look at [clap](https://docs.rs/clap/latest/clap/) or other libraries.
+
+## How to add your plugin
+
+1. Add your plugin as a dependency of the project, for instance with `cargo add`.
+2. Import the plugin's struct in `main.rs`.
+3. Add the plugin's type to the list of plugins (the `static_plugins!` macro) in `main.rs`.
+4. Compile everything together with `cargo build`. The binary is generated in `target/`. Note that, by default, you are in `debug` (unoptimized) mode.
+
+Pro-tip: you can compile and run quickly with `cargo run`. To pass arguments to the Alumet agent, use `--`. For instance, `cargo run -- plugins list`.
+
+For more information on plugins, read the [Alumet Developer Book](https://alumet-dev.github.io/developer-book/).
---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use std::time::Duration;
+
+use alumet::{
+    agent::{
+        self, config,
+        plugin::{PluginSet, UnknownPluginInConfigPolicy},
+    },
+    pipeline, static_plugins,
+};
+
+use plugin_csv::CsvPlugin;
+
+fn main() {
+    // Initialize the logger (otherwise you won't see much).
+    init_logger();
+
+    // Load the plugin metadata
+    let mut plugins = PluginSet::from(static_plugins![CsvPlugin]);
+
+    // Load the configuration file.
+    let general = || toml::Table::new(); // no default general options
+    let default_config_provider = config::AutoDefaultConfigProvider::new(&plugins, general);
+    let mut config = config::Loader::parse_file("alumet-config.toml")
+        .or_default(default_config_provider, true) // if the config file does not exist, generate it
+        .load() // load now
+        .expect("could not load config file");
+
+    // Apply the configuration to the plugins.
+    plugins
+        .extract_config(&mut config, true, UnknownPluginInConfigPolicy::Error)
+        .expect("invalid plugins config");
+
+    // Set up the measurement pipeline
+    let mut pipeline = pipeline::Builder::new();
+    pipeline.normal_threads(2); // Example setting: use 2 threads to run async pipeline elements
+
+    // Build and start the agent
+    let agent = agent::Builder::from_pipeline(plugins, pipeline)
+        .build_and_start()
+        .expect("startup failure");
+
+    // Run until shutdown (you can use Ctrl+C to initiate shutdown from the terminal)
+    agent
+        .wait_for_shutdown(Duration::MAX)
+        .expect("error while running");
+}
+
+pub fn init_logger() {
+    use env_logger::{Builder, Env};
+
+    Builder::from_env(Env::default().default_filter_or("info")).init();
+}
